fd2393dae23489e67690907ff46564e2ff367fc1
Edward Paler <paler.edward@gmail.com>
2021-11-19 11:56:09 +0800

    Updated fetch description

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -65,7 +65,7 @@ export default function Register(){
                          Swal.fire({
                              title: 'Registration Successful',
                              icon: "success", 
-                             text: 'Welcome to Zuitt!'
+                             text: 'Welcome to PieZada!'
                          })                          
                          
                          setRegister( true );
